4d21d62c347f7ae454e81dab334f1027bdbb5bf0
Prashanth Puranik <puranik@digdeeper.in>
2024-06-24 11:18:25 +0530

    object

---
+++ demos/day-01/01-basic-js/exercises/01-object-Movie.js
@@ -0,0 +1,23 @@
+/** 
+Create a movie object that represents details of your favorite movie.
+Suggested information to have in the object
+    - name
+    - cast (an array of strings with cast member's names)
+    - yearOfRelease
+    - boxOfficeCollection
+    
+    - Don't try now - addToCast( newMember ) that accepts a new cast member's name and adds to the cast array,
+    - Don't try now - addToCollection(amount) that accepts box office collections for a week and adds it to the current boxOfficeCollection.
+*/
+const sholay = {
+    name: "Sholay",
+    cast: [
+        "Amitabh",
+        "Dharmendra"
+    ],
+    yearOfRelease: 1975,
+    boxOfficeCollection: {
+        value: 20,
+        units: "crore"
+    }
+}
